eb97f4ebcaeb005fea4b899e78d52f9d62b8a007
hgmachine <hgmoura@yahoo.com>
2025-01-09 12:00:21 -0300

    Update bmvc_setup.sh

--- bmvc_setup.sh
+++ bmvc_setup.sh
@@ -1,3 +1,2 @@
-docker container stop mybmvci
-docker build -t bmvci .
-docker run -d --rm --name mybmvci -p 8080:8080 -v $(pwd):/app bmvci
+docker build -t bmetimg .
+docker run --name bmetaap -p 8080:8080 -v $(pwd):/bmeta bmetimg
